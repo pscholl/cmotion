ada542a682686141c8f446c07f93f8cf663da50a
Philipp M. Scholl <pscholl@ese.uni-freiburg.de>
2016-08-30 16:15:16 +0200

    can create matroska fiels now

--- srt/src/androidTest/java/de/uni_freiburg/es/sensorrecordingtool/FFMpegTest.java
+++ srt/src/androidTest/java/de/uni_freiburg/es/sensorrecordingtool/FFMpegTest.java
@@ -1,6 +1,7 @@
 package de.uni_freiburg.es.sensorrecordingtool;
 
 import android.content.Context;
+import android.os.Environment;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.MediumTest;
@@ -10,6 +11,8 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.Socket;
 
@@ -20,9 +23,16 @@ import java.net.Socket;
 @MediumTest
 public class FFMpegTest {
     Context c;
+    private String filename;
+    private File filepath;
 
     @Before public void setup() {
         c = InstrumentationRegistry.getContext();
+        filename = "test.wv";
+        filepath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
+
+        try { new File(filepath, filename).delete(); }
+        catch (Exception e) {}
     }
 
     @Test public void callFFMpeg() throws IOException, InterruptedException {
@@ -41,21 +51,35 @@ public class FFMpegTest {
         int e = p.waitFor();
         int i = p.getErrorStream().read(b);
 
-        Assert.assertTrue("return code is correct", e==1);
+        Assert.assertTrue(new String(b,0,i), e==1);
         Assert.assertTrue("ffprobe in output", new String(b, 0,i).contains("ffprobe"));
     }
 
-    @Test public void encodeSomeNumbersIntoSingleChannel() throws IOException, InterruptedException {
+    @Test public void encodeSomeNumbersIntoSingleChannelWavpacked() throws IOException, InterruptedException {
         byte[] b = new byte[4096];
-        FFMpegProcess p = FFMpegProcess.ffmpeg(c, "-f u8 -i tcp://localhost:%port?listen -f wav -y test.wav");
+        FFMpegProcess p = FFMpegProcess.ffmpeg(c, "-f u8 -i tcp://localhost:%port?listen -f wv -y "+filename);
 
         p.getOutputStream(0).write(b);
         p.getOutputStream(0).close();
 
         int e = p.waitFor();
         int i = p.getErrorStream().read(b);
-        System.err.println(new String(b, 0, i));
-        Assert.assertTrue("command failed, check the logs", e==0);
 
+        Assert.assertTrue(new String(b,0,i), e==0);
+        Assert.assertTrue(new File(filepath, filename).isFile());
+    }
+
+    @Test public void encodeSomeNumbersIntoSingleChannelMatroska() throws IOException, InterruptedException {
+        byte[] b = new byte[4096];
+        FFMpegProcess p = FFMpegProcess.ffmpeg(c, "-f u8 -i tcp://localhost:%port?listen -f matroska -y "+filename);
+
+        p.getOutputStream(0).write(b);
+        p.getOutputStream(0).close();
+
+        int e = p.waitFor();
+        int i = p.getErrorStream().read(b);
+
+        Assert.assertTrue(new String(b,0,i), e==0);
+        Assert.assertTrue(new File(filepath, filename).isFile());
     }
 }
